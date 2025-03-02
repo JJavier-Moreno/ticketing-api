6e9f1ac8ebdbc300cf9222844a87f3e4ed133d0b
JJavier-Moreno <josejavier.moreno.frz@gmail.com>
2025-03-02 13:29:44 +0100

    Se realiza conexión con mongodb

--- db.js
+++ db.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const DB_URL =
+  process.env.NODE_ENV === "test"
+    ? "mongodb://localhost:27017/ticketing-db-test"
+    : process.env.MONGO_URL || "mongodb://localhost:27017/ticketing-db";
+
 const connectDB = () => {
   try {
     mongoose
-      .connect(process.env.MONGO_URL)
+      .connect(DB_URL)
       .then(() => {
-        console.log("Conectado a la base de datos");
+        console.log(`Conectado a la base de datos ${DB_URL}`);
       })
       .catch((error) => {
         console.log(error);
