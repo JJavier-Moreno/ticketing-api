eee3d5acf226910965c11b8bdb2fc58c32fbb3dc
JJavier-Moreno <josejavier.moreno.frz@gmail.com>
2025-03-03 14:27:27 +0100

    Se crean los endpoints para los tickets

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./db.js";
 import userRouter from "./routes/userRoutess.js";
+import ticketRouter from "./routes/ticketRoutess.js";
 
 const app = express();
 
@@ -13,6 +14,7 @@ dotenv.config();
 connectDB();
 
 app.use(morgan("dev"));
-app.use("/api/user", userRouter);
+app.use("/api/users", userRouter);
+app.use("/api/tickets", ticketRouter);
 
 export default app;
---
+++ routes/ticketRoutess.js
@@ -0,0 +1,136 @@
+import express from "express";
+import Ticket from "../models/ticket.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const tickets = await Ticket.find({});
+
+    if (!tickets) {
+      return res.status(400).json({
+        status: "fail",
+        message: "No tickets found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      tickets,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: "fail",
+      message: error.message,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  
+  try {
+    //NO podemos utilizar findbyId ya que aqui lo que hace sería buscar por el _id que genera mongoose en la base de datos
+    const ticket = await Ticket.findOne({id});
+
+    if (!ticket) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Ticket not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      ticket,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: "fail",
+      message: error.message,
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { userId, title, description, priority, state } = req.body;
+
+  const ticket = new Ticket({
+    user: userId,
+    title,
+    description,
+    priority,
+    state,
+  });
+
+  try {
+    await ticket.save();
+
+    res.status(201).json({
+      status: "success",
+      ticket,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: "fail",
+      message: error.message,
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    const ticket = await Ticket.findOneAndUpdate({id}, updates, {
+      new: true,
+    });
+
+    if (!ticket) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Ticket not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      ticket,
+    });
+
+    await ticket.save();
+  } catch (error) {
+    res.status(500).send({
+      status: "fail",
+      message: error.message,
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params.id;
+
+  try {
+    const ticket = await Ticket.findByIdAndDelete(id);
+
+    if (!ticket) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Ticket not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      ticket,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: "fail",
+      message: error.message,
+    });
+  }
+});
+
+export default router;
