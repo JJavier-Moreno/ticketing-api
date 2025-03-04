1bb013d62ce6ad9b271e1a442c7ba19b24de6421
JJavier-Moreno <josejavier.moreno.frz@gmail.com>
2025-03-04 12:08:41 +0100

    Se crea un logger para obtener logs tanto de error como de info de los endpoints

---
+++ helpers/logger.js
@@ -0,0 +1,15 @@
+import winston from "winston";
+
+const logger = createLogger({
+    level: "error",
+    format: winston.format.combine( //Formato de los logs
+        winston.format.prettyPrint(), //Se le da un color al log
+        winston.format.timestamp(), 
+    ),
+    transports: [ //Son los archivos donde se guardan los logs
+        new winston.transports.File({filename: "error.log", level: "error"}),
+        new winston.transports.File({filename: "info.log", level: "info"})
+    ]
+})
+
+export default logger;
